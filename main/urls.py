6155395bb1c7eb9fb442b72887ded2eba0c80015
ranifaizah <rani.faizah@ui.ac.id>
2026-09-16 14:57:29 +0700

    fixed unknown function

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from main.views import create_experiences, show_main, show_experience,show_skills,create_skills,create_experiences
+from main.views import create_experiences, show_main, show_experiences,show_skills,create_skills,create_experiences
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
-    path("experience/", show_experience, name="show_experience"),
+    path("experience/", show_experiences, name="show_experiences"),
     path('skills/', show_skills, name='show_skills'),
     path("skills/add/", create_skills, name="create_skills"), 
     path("experiences/add/", create_experiences, name="create_experiences"), 
