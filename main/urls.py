a93dd1c52d7c048c817e842c6eb959fcae90e5cc
ranifaizah <rani.faizah@ui.ac.id>
2026-09-21 06:44:10 +0700

    made create project.html

--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,14 @@ urlpatterns = [
     path("experiences/add/", create_experiences, name="create_experiences"), 
     path("api/skills/", get_skills_json, name="get_skills_json"),
     path("skills/<uuid:skill_id>/delete/",delete_skill,name="delete_skill"),
-        path("experiences/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
+    path("experiences/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
+
+
+    path('projects/', show_projects, name='show_projects'),
+    path('projects/create/', create_project, name='create_project'),
+    path('projects/<uuid:id>/update/', update_project, name='update_project'),
+    path('projects/<uuid:id>/delete/', delete_project, name='delete_project'),
+    path('projects/json/', show_json, name='show_json'),
+    path('projects/json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
+    path('projects/json-view/', show_projects_json_view, name='show_projects_json_view'),
 ]
